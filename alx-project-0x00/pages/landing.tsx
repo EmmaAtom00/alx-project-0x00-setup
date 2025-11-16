61d1ab2d86d4edaef51b1b76c5c798abf4e17fcb
Emmanuel Olarewaju <emmanuelmark501@gmail.com>
2025-11-16 23:49:16 +0100

    advanced task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
@@ -5,6 +6,9 @@ const Landing: React.FC = () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title="Click me" styles="size-small rounded-sm" />
+      <Button title="Medium size" styles="size-medium rounded-md" />
+      <Button title="Large size" styles="size-large rounded-full" />
     </div>
   );
 };
