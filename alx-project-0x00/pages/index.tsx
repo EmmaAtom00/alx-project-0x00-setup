feee5a7c1195e4ca2089df79193d5cae74b40418
Emmanuel Olarewaju <emmanuelmark501@gmail.com>
2025-11-16 22:33:20 +0100

    setting up project directories

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+const Home: React.FC = () => {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">
+        Get Started
+      </button>
+    </main>
+  );
+};
 
-function index() {
-  return <div>index</div>;
-}
-
-export default index;
+export default Home;
